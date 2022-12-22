ab8de560a9c8c493413cf05946bb5f62cc36b0f0
Gundo Sifhufhi <sifhufhisg@gmail.com>
2022-12-22 14:01:55 +0200

    Require Postcode, Country ID, City to get Rates.

--- uafrica/Customshipping/view/frontend/web/js/model/shipping-rates-validation-rules.js
+++ uafrica/Customshipping/view/frontend/web/js/model/shipping-rates-validation-rules.js
@@ -10,7 +10,15 @@ define(
         return {
             getRules: function() {
                 return {
-
+                    'postcode': {
+                        'required': true
+                    },
+                    'country_id': {
+                        'required': true
+                    },
+                    'city': {
+                        'required': true
+                    }
                 };
             }
         };
