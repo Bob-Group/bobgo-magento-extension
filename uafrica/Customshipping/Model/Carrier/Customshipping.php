1c5536c54b39a2926d23c0bba7bad0dab2d5ee1c
Gundo Sifhufhi <sifhufhisg@gmail.com>
2022-12-28 16:29:32 +0200

    Refactoring

--- uafrica/Customshipping/Model/Carrier/Customshipping.php
+++ uafrica/Customshipping/Model/Carrier/Customshipping.php
@@ -544,7 +544,6 @@ class Customshipping extends AbstractCarrier implements CarrierInterface
             $method->setCarrierTitle('uafrica');
             $method->setMethod($code);
             $method->setMethodTitle($title['service_name']);
-            //Convert total price from cents to dollars
             $method->setPrice($title['total_price'] / 100);
             $method->setCost($title['total_price'] / 100);
             $result->append($method);
