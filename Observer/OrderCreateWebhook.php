84e19b42a5ef2b7ed67bdd2964ecab6bc2b30ed0
@ChristelLoftus <christel@bob.co.za>
2024-08-29 12:00:12 +0200

    cleanup

--- Observer/OrderCreateWebhook.php
+++ Observer/OrderCreateWebhook.php
@@ -3,25 +3,9 @@
 namespace BobGroup\BobGo\Observer;
 
 use Magento\Framework\Event\Observer;
-use Magento\Framework\Event\ObserverInterface;
-use Magento\Framework\HTTP\Client\Curl;
-use BobGroup\BobGo\Model\Carrier\UData;
-use Psr\Log\LoggerInterface;
-use Magento\Store\Model\StoreManagerInterface;
 
 class OrderCreateWebhook extends OrderWebhookBase
 {
-    protected Curl $curl;
-    protected LoggerInterface $logger;
-    protected StoreManagerInterface $storeManager;
-
-    public function __construct(LoggerInterface $logger, Curl $curl, StoreManagerInterface $storeManager)
-    {
-        $this->logger = $logger;
-        $this->curl = $curl;
-        $this->storeManager = $storeManager;
-    }
-
     public function execute(Observer $observer)
     {
         $order = $observer->getEvent()->getOrder();
